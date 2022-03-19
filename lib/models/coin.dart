2043f1c18b830eaae7eed342e4e9e1b57b0acf85
Matheus Miranda <matheus.sm86@gmail.com>
2022-03-19 14:01:23 -0300

    [FEAT] - Create Coin model

---
+++ lib/models/coin.dart
@@ -0,0 +1,29 @@
+class Coin {
+  String baseId;
+  String icon;
+  String name;
+  String symbol;
+  double price;
+  DateTime timestamp;
+  double changeHour;
+  double changeDay;
+  double changeWeek;
+  double changeMonth;
+  double changeYear;
+  double changeAll;
+
+  Coin({
+    required this.baseId,
+    required this.icon,
+    required this.name,
+    required this.symbol,
+    required this.price,
+    required this.timestamp,
+    required this.changeHour,
+    required this.changeDay,
+    required this.changeWeek,
+    required this.changeMonth,
+    required this.changeYear,
+    required this.changeAll,
+  });
+}
